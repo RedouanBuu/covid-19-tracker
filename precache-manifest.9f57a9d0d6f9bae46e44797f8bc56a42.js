4b4b3d8f3da36b5857cf595ba8acf3a618d3e528
RedouanBuu <redouan.bouzid@student.hu.nl>
2021-02-22 10:39:00 +0100

    Updates

---
+++ precache-manifest.9f57a9d0d6f9bae46e44797f8bc56a42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb33fa8ead8b48d9a4a7e921f47b70fe",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "cd740a35c2879a7535e8",
+    "url": "/covid-19-tracker/static/css/main.1effbc15.chunk.css"
+  },
+  {
+    "revision": "eacd77452b7027f62f1a",
+    "url": "/covid-19-tracker/static/js/2.06717c4a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.06717c4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd740a35c2879a7535e8",
+    "url": "/covid-19-tracker/static/js/main.648eb4f4.chunk.js"
+  },
+  {
+    "revision": "62e2170e4111863d66c2",
+    "url": "/covid-19-tracker/static/js/runtime-main.68bee0c4.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/image.d7265326.png"
+  }
+]);
